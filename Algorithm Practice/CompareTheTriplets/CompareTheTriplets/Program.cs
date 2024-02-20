0b6865e2acabac1a2fba2b6d113aed85cb402216
leonardo.costa <lcampelo17@gmail.com>
2024-02-19 21:28:39 -0300

    Exercise CompareTheTriplets

---
+++ Algorithm Practice/CompareTheTriplets/CompareTheTriplets/Program.cs
@@ -0,0 +1,36 @@
+﻿class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> a = DataManipulator.GenerateValues();
+
+        List<int> b = DataManipulator.GenerateValues();
+        
+        List<int> result = DataManipulator.compareTriplets(a, b);
+
+        Console.WriteLine($" {String.Join($" ", a)} \n {String.Join(" ", b)} ");
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
+
+class DataManipulator
+{
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        List<int> score = new List<int>();
+        
+        List<int> comparison = a.Zip(b, (x, y) => x > y ? 1 : (x == y ? 0 : -1)).ToList();
+        
+        score.Add(comparison.Where(x => x == 1).Sum());
+        score.Add(Math.Abs(comparison.Where(x => x == -1).Sum()));
+
+        return score;
+    }
+
+    public static List<int> GenerateValues()
+    {
+        Random random = new Random();
+
+        return Enumerable.Range(0, 3).Select(x => random.Next(1, 3)).ToList();
+    }  
+}

5450f6925969106c241d6afb4ab0004e4d8ab582
leo <lcampelo17@gmail.comgit config --global user.email lcampelo17@gmail.comgit config --global user.email lcampelo17@gmail.com>
2024-03-21 21:25:50 -0300

    Criação IOC

---
+++ CleanArchitecture/CleanArchMvc/CleanArchMvc.Infra.IoC/DependecyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>                              //Registra o contexto
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")     //Define o Banco de dados e string de conexão
+                ,b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));   //Informa que a migração vai ficar no arquivo de contexto (ApplicationDbContext)
+
+            //Registrando os serviços de repositório
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
